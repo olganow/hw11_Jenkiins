8275f3958c21eef1d5d9bc99bd1d649915aac116
olganow <olganow@yandex.ru>
2025-01-08 21:45:42 +0300

    allure report added

--- src/test/java/tests/TestBase.java
+++ src/test/java/tests/TestBase.java
@@ -1,7 +1,10 @@
 package tests;
 
 import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.logevents.SelenideLogger;
+import io.qameta.allure.selenide.AllureSelenide;
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 
 import static constants.Constants.BASE_URL;
 
@@ -11,5 +14,11 @@ public class TestBase {
     static void beforeAll() {
         Configuration.baseUrl = BASE_URL;
         Configuration.pageLoadStrategy = "eager";
+
+    }
+
+    @BeforeEach
+    void setupTest() {
+        SelenideLogger.addListener("AllureSelenide", new AllureSelenide());
     }
 }
